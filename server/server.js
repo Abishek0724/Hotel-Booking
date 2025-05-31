c225faf11c1729f9dff75127a8db27cff3afaf78
Abishek0724 <Palaniabishek24@gmail.com>
2025-05-31 17:21:28 +0530

    bugss

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 // server.js
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import "dotenv/config"; // Make sure this is at the very top to load environment variables
 import bodyParser from "body-parser";
 import connectDB from "./configs/db.js";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
-connectDB();
+// Load environment variables from .env file
+// Ensure this is the first thing that runs to make process.env available
+// If using 'dotenv/config', it automatically loads. Otherwise, use:
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+connectDB(); // Attempt to connect to the database
+
 const app = express();
 app.use(cors());
 
@@ -14,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 // Clerk webhook uses raw body
+// IMPORTANT: bodyParser.raw must be used for Clerk webhooks before express.json()
+// for this specific route, as Clerk sends raw JSON that needs verification.
 app.post("/api/clerk", bodyParser.raw({ type: "application/json" }), clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is Working"));
